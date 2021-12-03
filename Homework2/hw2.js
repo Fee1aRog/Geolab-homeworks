17396118423201fd1cd4287a15ac163f2e96e296
George <barishvili2001@gmail.com>
2021-12-03 23:26:22 +0400

    done2

--- Homework2/hw2.js
+++ Homework2/hw2.js
@@ -1,2 +1,23 @@
-console.log('hello');
-//komentari
+// //1
+// const array = [5, 35, 65, 98, 1];
+// console.log(Math.max(...array));
+// console.log(Math.min(...array));
+// //1
+
+
+//2
+// function Numbersquare(){
+//     let num1=4;
+//     let num2=3;
+//     console.log(Math.pow(num1, num2));
+// }
+// Numbersquare();
+//2
+
+
+//3
+let N1=2;
+let N2=3;
+()=>(Math.pow(N1, N2))  //აქაც და წინა დავალებაშიც შეგვიძლია რიცხვები პირდაპირ (N1, N2)-ის მაგივრად ჩავწეროთ და ასევე შესაძლებელია ეს დავალება გაკეთდეს "**"-ით მაგრამ ეს გზა ავირჩიე.
+console.log(Math.pow(N1, N2));
+//3
