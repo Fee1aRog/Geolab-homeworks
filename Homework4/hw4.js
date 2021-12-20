acafe7f821494a7a8506a6af410be4be64621135
Fee1aRog <90037916+Fee1aRog@users.noreply.github.com>
2021-12-21 03:07:23 +0400

    ss

--- Homework4/hw4.js
+++ Homework4/hw4.js
@@ -1,1 +1,22 @@
-console.log('Hello');
+const arr =[
+    {
+        username: 'Sandro',
+        age: 17
+    },
+    {
+        username: 'Giorgi',
+        age: 20
+    },
+    {
+        username: 'Mariami',
+        age: 27 
+    }
+];
+
+const mdiv = document.querySelector('.mdiv');
+const ul = document.createElement('ul');
+const li = document.createElement('li');
+console.log(ul);
+mdiv.appendChild(ul);
+ul.appendChild(li);
+li.textContent= arr;
---
+++ Lecture7/lecture7.js
@@ -0,0 +1,1 @@
+"https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"
